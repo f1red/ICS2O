85e56e6f4022643e8b157a2b50b46581f6244c30
f1red <ktran13@ocdsb.ca>
2017-03-23 14:13:16 -0400

    Update sketch.js

--- p5/House/sketch.js
+++ p5/House/sketch.js
@@ -25,21 +25,23 @@ fill(255, 255, 255);
 ellipse (10, 20, 83, 85); // moon
 
 // second house 
-var houseXS = 8;
-var houseYS = 230;
-var houseSizeWS = 125;
-var windowWidthS = houseSizeWS/4.73;
-var houseSizeHS = 150;
+var houseX = 12;
+var houseY = 227;
+var houseSizeW = 125;
+var windowWidth = houseSizeW/4.73;
+var houseSizeH = 152;
+strokeWeight (2);
 fill (209, 88, 88);
-rect (houseXS, houseYS, houseSizeWS, houseSizeHS); // house shape
+rect (houseX, houseY, houseSizeW, houseSizeH); // house shape
 fill(0, 0, 0);
-triangle(houseXS, houseYS, houseXS + houseSizeWS, houseYS, houseXS + houseSizeWS/2, houseYS - houseSizeHS/6); // roof
+triangle(houseX, houseY, houseX + houseSizeW, houseY, houseX + houseSizeW/2, houseY - houseSizeH/6); // roof
 fill (255, 255, 255);
-rect (houseXS + houseSizeWS/3.91, houseYS + houseSizeHS/1.51, houseSizeWS/3.8, houseSizeHS/3 ); // door
+rect (houseX + houseSizeW/3.91, houseY + houseSizeH/1.51, houseSizeW/3.8, houseSizeH/3 ); // door
 fill(72, 168, 242);
-rect (houseXS + houseSizeWS/1.5, houseYS + houseSizeHS/3, windowWidthS, houseSizeHS/1.65); // big window
-rect (houseXS + houseSizeWS/10, houseYS + houseSizeHS/12, houseSizeWS/5, houseSizeHS/6); // small window
-rect (houseXS + houseSizeWS/3.2, houseYS + houseSizeHS/1.4, houseSizeWS/6.25, houseSizeHS/4); // door window
+stroke (255 ,255, 255);
+rect (houseX + houseSizeW/1.5, houseY + houseSizeH/3, windowWidth, houseSizeH/1.65); // big window
+rect (houseX + houseSizeW/10, houseY + houseSizeH/12, houseSizeW/5, houseSizeH/6); // small window
+rect (houseX + houseSizeW/3.2, houseY + houseSizeH/1.4, houseSizeW/6.25, houseSizeH/4); // door window
 fill(55, 186, 123);
 ellipse (200, 430, 806, 113);
 }
